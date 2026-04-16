40b6680b960fc75f168a556fcdbe891836a0fd98
Markus Wals <markus@wals.pro>
2026-04-16 23:42:28 +0200

    fix(custom-api): cast method to IHttpRequestMethods, fix handleBinaryDownload call signature

--- nodes/Weclapp/descriptions/CustomApiDescription.ts
+++ nodes/Weclapp/descriptions/CustomApiDescription.ts
@@ -1,4 +1,4 @@
-import type { IDataObject, IExecuteFunctions, INodeExecutionData, INodeProperties } from 'n8n-workflow';
+import type { IDataObject, IExecuteFunctions, IHttpRequestMethods, INodeExecutionData, INodeProperties } from 'n8n-workflow';
 import { NodeOperationError } from 'n8n-workflow';
 import { handleBinaryDownload, weclappApiRequest } from '../GenericFunctions';
 
@@ -227,7 +227,7 @@ export async function executeCustomApiCall(
 	this: IExecuteFunctions,
 	itemIndex: number,
 ): Promise<INodeExecutionData> {
-	const method = this.getNodeParameter('method', itemIndex) as string;
+	const method = this.getNodeParameter('method', itemIndex) as IHttpRequestMethods;
 	const entityPath = this.getNodeParameter('entityPath', itemIndex) as string;
 	const entityId = this.getNodeParameter('entityId', itemIndex, '') as string;
 	const action = this.getNodeParameter('action', itemIndex, '') as string;
@@ -270,8 +270,7 @@ export async function executeCustomApiCall(
 	}
 
 	if (returnBinary) {
-		const response = await weclappApiRequest.call(this, method, endpoint, body, qs);
-		const binaryItem = await handleBinaryDownload.call(this, response);
+		const binaryItem = await handleBinaryDownload.call(this, method, endpoint, 'download', body, qs);
 		if (binaryPropertyName !== 'data' && binaryItem.binary?.data) {
 			binaryItem.binary[binaryPropertyName] = binaryItem.binary.data;
 			delete binaryItem.binary.data;
